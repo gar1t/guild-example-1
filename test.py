4a01360da587edc6caa5e9fec0f7a72478c05e01
Garrett Smith <g@rre.tt>
2020-10-15 17:22:00 -0500

    Simplify example

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 
-data = "foo.txt"
+data = "data/foo.txt"
 
 print("Contents of data file %s" % data)
-print(open(os.path.join("data", data)).read().rstrip())
+print(open(data).read().rstrip())
